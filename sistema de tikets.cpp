a6feecf516c5678ef3840fa6a9acddfedbe7425e
Crisrod231223 <66752368+Crisrod231223@users.noreply.github.com>
2020-07-26 00:37:30 -0500

    Add files via upload
    Codigo c++

---
+++ sistema de tikets.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+   int np=0,vp=0,t=0,mp=0,st=0,destino,valortik,vt;
+   
+   
+   cout<<"ingrese su destino";
+   cin<<destino;
+   cout<<"ingrese n pasajes";
+   cin>>np;
+
+do{
+	np++;
+	cout<<"desea otro tiquete"<<"digite 1 para si 2 para no";
+	cin>>t;
+}
+ while{ (t==1)
+    cout<<"ingrese n pasajes"<<endl;
+	cin>>np;
+	ccout<<"digite valor del tikete";
+	cin>>valortik;
+    (t==2)
+     cout<<"digite meotodo de pago "<<"1 efectivo 2 tarjt credito";
+	 cin>>mp;
+   
+while{
+	 (mp==1 np*valortik*0,25)
+	  cin>>vt;
+	  cout<<"su compra es de";
+	  cin>>vt;
+	   (mp==2*np*valortik)
+	    cin>>vt
+	    cout<<" su compra es de";
+	    cin>>vt
+}
+
+return=0
+}
